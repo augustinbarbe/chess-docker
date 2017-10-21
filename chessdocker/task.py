cf8b7e4242f3cd2d6cdbbe2a9d5bfd0fe202809e
Augustin Barbe <augustin.barbe@gmail.com>
2017-10-21 16:13:55 +0200

    docker connection

--- chessdocker/task.py
+++ chessdocker/task.py
@@ -3,6 +3,11 @@ import docker
 
 from . import celery
 
+docker_client = docker.DockerClient()
+
 @celery.task
 def launch_container():
+    """ Test docker"""
     print(docker_client.images.list())
+    print(docker_client.containers.\
+                    run("ubuntu", "echo hello world"))
